ede007b8e1436362c78eb092e611e1d63e7e298f
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-11-13 18:42:05 +0700

    Delete unused code

--- ESP32-IoT-Monitoring-System/src/main.cpp
+++ ESP32-IoT-Monitoring-System/src/main.cpp
@@ -9,27 +9,14 @@
 void setup()
 {
   Serial.begin(115200);
-  // check_info_File(0); Need to read here
 
   xTaskCreate(rgb_led_task, "RGB Led Task", 2048, NULL, 2, NULL);
   xTaskCreate(main_server_task, "Main Server Task" ,8192  ,NULL  ,2 , NULL);
   xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
-  // xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
 }
 
 void loop()
 {
-  // if (check_info_File(1))
-  // {
-  //   if (!Wifi_reconnect())
-  //   {
-  //     Webserver_stop();
-  //   }
-  //   else
-  //   {
-  //     //CORE_IOT_reconnect();
-  //   }
-  // }
-  // Webserver_reconnect();
+  
 }
