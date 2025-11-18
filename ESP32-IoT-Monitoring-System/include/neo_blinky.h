e303dba053525deb2364d85ce9b58cd4ebe1b07f
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-11-18 19:09:19 +0700

    Revert "Merge remote-tracking branch 'origin/main' into Fix_serial_bug"
    This reverts commit ffc4f6efdd2881164101254b1c0cc73f97c9fcfa, reversing
    changes made to 58a71c02f879488b655b992df27d0a2987400d2b.

---
+++ ESP32-IoT-Monitoring-System/include/neo_blinky.h
@@ -0,0 +1,13 @@
+#ifndef __NEO_BLINKY__
+#define __NEO_BLINKY__
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#include "global.h"
+
+#define NEO_PIN 45
+#define LED_COUNT 1 
+
+void neo_led_task(void *pvParameters);
+
+#endif
